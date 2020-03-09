26e351be227c475acdf15a967e1ccbffdd79a634
LaurieSue <pink.bee.me@outlook.com>
2020-03-09 16:05:54 -0500

    problem...asking for help

--- JavaScript/JavaScript_1.js
+++ JavaScript/JavaScript_1.js
@@ -2,6 +2,13 @@ function Actor_Function() {
     var Actor_Output;
     var Actors = document.getElementById("Actor_Choice").value;
     var Actor_String = " is a great actor!!!";
+    var Go = function(){
+        var A = document.getElementsByClassName("Instruction");
+        A[0].innerHTML = "<strong>Choose your favorite actor from the list below:</strong>" 
+        A[1].innerHTML = "<strong>Enter your choice below:</strong>";
+        };
+
+   
     switch(Actors) {
         case "Keanu Reeves":
         Actor_Output = "Keanu Reeves" + Actor_String;
@@ -29,6 +36,13 @@ function Actor_Function() {
         break;
         default:
         Actor_Output = "Although many are 'GREAT',... Please choose one from the list provided (check your spelling,please)."
+    
+    ;
+
+    if (Actors_Output = "Although many are 'GREAT',... Please choose one from the list provided (check your spelling,please).") {
+    document.getElementById("ReDo").innerHTML = Go;
+
+    
     }
-    document.getElementById("Output").innerHTML = Actor_Output;
-    }
+}
+}
