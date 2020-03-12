4c96ce4c54eb2595d60fe667ae0abcaf15fecd93
LaurieSue <pink.bee.me@outlook.com>
2020-03-12 03:13:37 -0500

    added an output for unsuccessful second input by user

--- JavaScript/JavaScript_1.js
+++ JavaScript/JavaScript_1.js
@@ -9,7 +9,7 @@ var Actors2;
 var A = [];
    
 
-    function Actor_Function() { 
+function Actor_Function() { 
 
         switch(Actors || Actors2){ 
             case "Keanu Reeves":
@@ -43,7 +43,7 @@ var A = [];
      if (Actor_Output == (Actors + Actor_String)) {
 
          document.getElementById("Output").innerHTML = Actor_Output;
-         document.getElementById("End").innerHTML = "That Was Fun!!!";
+         document.getElementById("End").innerHTML = "That Was Fun!!! Thank you.";
          
         } 
 
@@ -59,26 +59,34 @@ var A = [];
         A[0].innerHTML = "<p style ='color:#fff';>" + choose.bold() +"</p>";
         A[1].innerHTML = "<p style ='color:#fff';>" + enter.bold() + "</p>";
        
-            {
-                var another_Chance = "<input id='Actor_Choice2' type='text'></input>";
-                var ReDo_Button = "<button onclick='Actors2()'>Click Here To Re-Submit Your Choice</button>";
+        {
+            var another_Chance = "<input id='Actor_Choice2' type='text'></input>";
+            var ReDo_Button = "<button onclick='Actors2()'>Click Here To Re-Submit Your Choice</button>";
         
-                document.getElementById("Second_Chance").innerHTML = another_Chance;
-                document.getElementById("ReDo").innerHTML = ReDo_Button;
+            document.getElementById("Second_Chance").innerHTML = another_Chance;
+            document.getElementById("ReDo").innerHTML = ReDo_Button;
 
-                 Actors2 = function() {
+                Actors2 = function() {
 
-                        Actors = document.getElementById("Actor_Choice2").value;
+                    Actors = document.getElementById("Actor_Choice2").value;
 
-                        Actor_Function();
+                    Actor_Function();
 
-                            if (Actor_Output == (Actors + Actor_String))  {
+                        if (Actor_Output == (Actors + Actor_String)) {
 
-                            document.getElementById("Output3").innerHTML = "Wow!!! " + Actors + "!!!! That's our FAVORITE TOO!!!";
+                                document.getElementById("Output3").innerHTML = "Wow!!! " + Actors + "!!!! That's our FAVORITE TOO!!!";
 
-                            document.getElementById("End").innerHTML = "We appreciate your participation. Thank You!!!"; 
-                        }
+                                document.getElementById("End").innerHTML = "We appreciate your participation. Thank You!!!"; 
+                            }
+
+                        else {
+
+                                document.getElementById("Output4").innerHTML = "Thank You for Your Input! We will take your suggestion into consideration...<br> Who Knows???<br><br> ...maybe, " + Actors +" will be on the NEXT Edition Of Our FAVORITE ACTORS LIST!!!<br><br>Thank you for your time and thoughtful contribution...GOOD-BYE!!!"; 
+                            }
+                        
+                        
                 }
-            }
         }
     }
+}
+    
