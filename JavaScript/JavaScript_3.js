8027c50d1f9d1be36f3bff0ec3aa31ade63509f1
LaurieSue <pink.bee.me@outlook.com>
2020-05-17 23:18:47 -0500

    won't work...headache!

--- JavaScript/JavaScript_3.js
+++ JavaScript/JavaScript_3.js
@@ -1,67 +1,100 @@
-let game= document.getElementsByClassName("myGameList");
-console.log(game);
-let gameChoice= this.onclick.id
-
-function Pick_Me(_gameChoice) {
-   
 
 
 
-    game[0]=document.getElementById("BattleZone");
-    game[1]=document.getElementById("CrashBandicoot");
-    game[2]=document.getElementById("OddWorld");
-    game[3]=document.getElementById("GodOfWar");
-    game[4]=document.getElementById("Joust");
-    game[5]=document.getElementById("LittleBigPlanet");
-    game[6]=document.getElementById("Defender");
-    game[7]=document.getElementById("SpaceAce");
-    game[8]=document.getElementById("SpaceInvaders");
-    game[9]=document.getElementById("Centipede");
-    game[10]=document.getElementById("MediEval");
-   
-    i= game.indexOf
 
-switch (gameChoice) {
 
-    case game[0]:
-        if (game[0].id == "BattleZone"){
-        document.getElementById("Video_Clip").innerHTML ="<iframe class='gameClip' autoplay controls src='https://www.youtube.com/embed/Ctr54kopo8I'><p>'The  browser is unable to  play the video clip at this time.'</p> </iframe>"};
+var  gameChoice = this.onclick;
+var vidClipArray = [
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/Ctr54kopo8I'><p>'The  browser is unable to  play the video clip at this time.'</p> </iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/PUr6JxzkzOc'  allow='accelerometer; autoplay;picture-in-picture'><p>'The  browser is unable to  play the video clip at this time.'</p> </iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/bLT-hCj8bTA?start=51' allow='accelerometer;autoplay;picture-in-picture'><p>'The  browser is unable to  play the video clip at this time.'</p></iframe>",
+
+      "<iframe class='gamePlay' autoplay controls  src='https://www.youtube.com/embed/CsSA1sK2oNc?start=51' allow='accelerometer; autoplay; picture-in-picture'><p>'The  browser is unable to  play the video clip at this time.'</p></iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/-4l6_7FDtrk?start=51' allow='accelerometer; autoplay; picture-in-picture'><p>'The  browser is unable to  play the video clip at this time.'</p></iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/X-L80KM9gM8?start=18' allow='accelerometer; autoplay; picture-in-picture'>The  browser is unable to  play the video clip at this time</iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/Ctr54kopo8I'>The  browser is unable to  play the video clip at this time. </iframe>",  
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/FYKzlKZzGZc?' allow='accelerometer; autoplay;  picture-in-picture'>The  browser is unable to  play the video clip at this time.</iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/FnF_wy-G6DY?start=18' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'>The  browser is unable to  play the video clip at this time.</iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/Ctr54kopo8I' allow = 'accelerometer;autoplay;picture-in-picture'>The  browser is unable to  play the video clip at this time </iframe>",
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/xrd8rniZYP8?' allow='accelerometer; autoplay; picture-in-picture'>'The  browser is unable to  play the video clip at this time.'</iframe>",
+
+
+      "<iframe class='gamePlay' autoplay controls src='https://www.youtube.com/embed/TXoDaas4XYM?' allow='accelerometer; autoplay; picture-in-picture'>'The  browser is unable to  play the video clip at this time.</iframe>",
+];
+
+
+
+
+
+
+
+
+function PickedOnClick() {
+
+  gameChoice = document.getElementsById
+
+
+  console.log(gameArray.length);
+  console.log(vidClipArray.length);
+
+ var gameVid = function() {
+    var x =  game.findIndex(gameChoice);
+    var y =  x;
+    document.getElementById("Video_Clip").innerHTML = vidClip[y];
+};
+ 
+  
+  switch (gameChoice) {
+    case "0_BattleZone":
+      document.getElementById("0_BattleZone").addEventListener("click", gameVid());
+      
+       break;
+    case "1_CrashBandicoot":
+        document.getElementById("Video_Clip").innerHTML = game[1];
         break;
-
-    case game[1]:
-        document.getElementById("Video_Clip").innerHTML = "<iframe class='gameClip' autoplay controls src='https://www.youtube.com/embed/PUr6JxzkzOc' >The browser is unable to  play the video clip.</iframe>";
+    case  "2_OddWorld":
+        document.getElementById("Video_Clip").innerHTML = gameClip[2];
         break;
-    case game[2]:
-        document.getElementById("Video_Clip").innerHTML ="<iframe autoplay controls src='https://www.youtube.com/embed/B8XTi2iWRY8' >The  browser is unable to  play the video clip at this time.</iframe>";
+    case  "3_GodOfWar":
+        document.getElementById("Video_Clip").innerHTML = gameClip[3];
         break;
-
-
-
-
-
+    case "4_Joust":
+        document.getElementById("Video_Clip").innerHTML = gameClip[4];
+        break;
+    case  "5_LittleBigPlanet":
+         document.getElementById("Video_Clip").innerHTML = gameClip[5];
+         break;
+    case  "6_Defender":
+        document.getElementById("Video_Clip").innerHTML = gameClip[6];
+        break;
+    case  "7_DragonsLair":
+        document.getElementById("Video_Clip").innerHTML = gameClip[7];
+        break;
+    case  "8_DigDug":
+       document.getElementById("Video_Clip").innerHTML = gameClip[8];
+       break;
+    case  "9_QBert":
+        document.getElementById("Video_Clip").innerHTML = gameClip[9];
+        break;
+    case  "10_MediEvil":
+        document.getElementById("Video_Clip").innerHTML = gameClip[10];
+        break;
+   default:
+   document.getElementById("Error").innerHTML = "Please come back later...the site is having routine maintenance services at this time. We apologize for this inconvenience."
+  }
 
 }
-  
-// when I did these if/else statements the BattleZone video would load and play, but none of the other options would work --so then i started trying the stuff up there (the code above this). Then I couldn't get anything to work and the error I get says that my Pick_Me function is  not defined...uggghhh! I was thinking that the switch/case method would work but now im not sure what to do...So I am stopping and asking for help because I am just making things worse!
 
-   if  (game ="li#BattleZone.myGameList") {
-    document.getElementById("Video_Clip").innerHTML ="<iframe class='gameClip' autoplay controls src='https://www.youtube.com/embed/Ctr54kopo8I'><p>'The  browser is unable to  play the video clip at this time.'</p> </iframe>"
-   }
-   
-    else if (game="li#CrashBandicoot.myGameList") {
-       document.getElementById("Video_Clip").innerHTML = "<iframe class='gameClip' autoplay controls src='https://www.youtube.com/embed/PUr6JxzkzOc' >The browser is unable to  play the video clip.</iframe>"
-   }
-   else if (game="li#OddWorld.myGameList") {
-    document.getElementById("Video_Clip").innerHTML ="<iframe autoplay controls src='https://www.youtube.com/embed/B8XTi2iWRY8' ><p>'The  browser is unable to  play the video clip at this time.'</p>></iframe>"
-    }
-   else if (game="li#GodOfWar.myGameList") {
-    document.getElementById("Video_Clip").innerHTML = "<iframe class= 'gameClip' autoplay controls src='https://www.youtube.com/embed/2Ga2Dtkg92I'><p>'The  browser is unable to  play the video clip at this time.'</p>></iframe>"
-   }
-   
-    else if (game="li#Joust.myGameList") {
-        document.getElementById("Video_Clip").innerHTML = "<iframe class= 'gameClip' autoplay controls src='https://www.youtube.com/embed/2Ga2Dtkg92I'><p>'The  browser is unable to  play the video clip at this time.'</p>></iframe>"
-    }
-        else if (game="li#Joust.myGameList") {
-            document.getElementById("Video_Clip").innerHTML = "<iframe class= 'gameClip' autoplay controls src='https://www.youtube.com/embed/2Ga2Dtkg92I'><p>'The  browser is unable to  play the video clip at this time.'</p>></iframe>"
-        }
-    }
+
+ 
+// when I did these if/else statements the BattleZone video would load and play, but none of the other options would work --so then i started trying the stuff up there (the code above this). Then I couldn't get anything to work and the error I get says that my Pick_Me function is  not defined...uggghhh! I was thinking that the switch/case method would work but now im not sure what to do...So I am stopping and asking for help because I am just making things worse!
